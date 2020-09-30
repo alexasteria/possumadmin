e713d52ab8b81cb37fa5d08464d8c9776379dc48
Alex <alexasteria@icloud.com>
2020-09-30 15:43:43 +0300

    Updates

---
+++ precache-manifest.41d768760c9598cb8ea03f19e1ca803c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6057937a48f04919b28c3e2f9b45ebd",
+    "url": "/possumadmin/index.html"
+  },
+  {
+    "revision": "563d6f76abe5ec83291d",
+    "url": "/possumadmin/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "f095b93151235d8d7285",
+    "url": "/possumadmin/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "563d6f76abe5ec83291d",
+    "url": "/possumadmin/static/js/2.32135195.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/possumadmin/static/js/2.32135195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f095b93151235d8d7285",
+    "url": "/possumadmin/static/js/main.46ea9a9c.chunk.js"
+  },
+  {
+    "revision": "4f3c358f408854dca92a",
+    "url": "/possumadmin/static/js/runtime-main.6fdb2a91.js"
+  }
+]);
